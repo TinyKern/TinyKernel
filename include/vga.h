7e9bd394e191e2c811cf884100fd8a84d3e2605c
bobrossrtx <bobrossrtx@gmail.com>
2021-07-08 19:02:01 +0100

    Moved VGA stuff to vga.h

---
+++ include/vga.h
@@ -0,0 +1,30 @@
+#ifndef VGA_H
+#define VGA_H
+
+#include "kernel.h"
+
+#define VGA_ADDRESS 0xB8000
+#define BUFSIZE 2200
+
+uint16* vga_buffer;
+
+enum vga_color {
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    MAGENTA,
+    BROWN,
+    GREY,
+    DARK_GREY,
+    BRIGHT_BLUE,
+    BRIGHT_GREEN,
+    BRIGHT_CYAN,
+    BRIGHT_RED,
+    BRIGHT_MAGENTA,
+    YELLOW,
+    WHITE
+};
+
+#endif

5a688b3b551af5e7140ae62393cbba1a844f2344
bobrossrtx <bobrossrtx@gmail.com>
2021-07-09 12:46:25 +0100

    Moved types to types.h

---
+++ include/sys/types.h
@@ -0,0 +1,25 @@
+/**
+ * @file types.h
+ * @author Owen Boreham (owenkadeboreham@gmail.com)
+ * @version 0.1.3
+ * @date 2021-07-09
+ * 
+ * @copyright Copyright (c) 2021 TinyKernel
+ * This file is part of TinyKernel which is released
+ * under Apache License 2.0.  See file LICENSE or go
+ * to https://www.apache.org/licenses/LICENSE-2.0 for
+ * full license details.
+ */
+
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+#define NULL 0
+#define FALSE 0
+#define TRUE 1
+
+#endif

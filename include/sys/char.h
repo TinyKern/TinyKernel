2b78b290c76e1dfacdca5e059543605bab0ae18f
bobrossrtx <bobrossrtx@gmail.com>
2021-07-09 12:46:17 +0100

    Created char.h + created get_ascii_char function

---
+++ include/sys/char.h
@@ -0,0 +1,22 @@
+/**
+ * @file char.h
+ * @author Owen Boreham (owenkadeboreham@gmail.com)
+ * @version 0.1.1
+ * @date 2021-07-09
+ * 
+ * @copyright Copyright (c) 2021 TinyKernel
+ * This file is part of TinyKernel which is released
+ * under Apache License 2.0.  See file LICENSE or go
+ * to https://www.apache.org/licenses/LICENSE-2.0 for
+ * full license details.
+ */
+
+#ifndef CHAR_H
+#define CHAR_H
+
+#include "types.h"
+#include <kernel/keyboard.h>
+
+extern char get_ascii_char(uint8);
+
+#endif

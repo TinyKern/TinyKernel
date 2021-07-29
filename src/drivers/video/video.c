030e10ddf25ab01f1e914ba7e964611163117ee2
bobrossrtx <bobrossrtx@gmail.com>
2021-07-29 09:45:09 +0100

    Added copyright

--- src/drivers/video/video.c
+++ src/drivers/video/video.c
@@ -1,3 +1,16 @@
+/**
+ * @file video.c
+ * @author Owen Boreham (owenkadeboreham@gmail.com)
+ * @version 0.1.2
+ * @date 2021-07-06
+ * 
+ * @copyright Copyright (c) 2021 TinyKernel
+ * This file is part of TinyKernel which is released
+ * under Apache License 2.0.  See file LICENSE or go
+ * to https://www.apache.org/licenses/LICENSE-2.0 for
+ * full license details.
+ */
+
 #include <drivers/video/video.h>
 #include <drivers/vga/vga.h>
 #include <kernel/cpu/ports.h>
